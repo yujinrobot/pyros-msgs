8373000d1c11bf37eef91647ac18b73547a00eee
alexv <asmodehn@gmail.com>
2016-09-20 18:45:17 +0900

    WIP. commit before changing internal dict representation of optional messages

--- src/pyros_msgs/_punch.py
+++ src/pyros_msgs/_punch.py
@@ -22,38 +22,61 @@ from .msg import (
     # None
 )
 
+import genpy
+import std_msgs.msg
 
-def duck_punch(msg_mod):
+
+def duck_punch(msg_mod, default_data_value = None):
     def init_punch(self, *args, **kwds):
         __doc__ = msg_mod.__init__.__doc__
         # excepting when passing initialized_. it is meant to be an internal field.
         if 'initialized_' in kwds.keys():
             raise AttributeError("The field 'initialized_' is an internal field of {0} and should not be set by the user.".format(msg_mod._type))
-        if args or kwds:
-            super(msg_mod, self).__init__(*args, **kwds)
+        if args:  # the args for super(msg_mod, self) are fixed to the slots in ros messages
+            # so we can change it to kwarg to be more accepting (and more robust for changes)
+            kwds.update(zip([s for s in self.__slots__ if s != 'initialized_'], args))
+
+        if kwds:
+            # special case for string type(to support unicode kwds)
+            for s, st in zip(self.__slots__, self._slot_types):
+                if st == 'string':
+                    kwds[s] = str(kwds.get(s, ""))
+
+            # ROS messages accept either args or kwargs, not both
+            super(msg_mod, self).__init__(**kwds)
             # initialized value depends on all field assigned
-            if self.data is None:
-                self.data = False
+            if 'data' in self.__slots__ and self.data is None:
+                self.data = default_data_value
                 self.initialized_ = False
             else:
                 self.initialized_ = True
         else:
             self.initialized_ = False
-            self.data = False
+            if 'data' in self.__slots__:
+                self.data = default_data_value
 
     # duck punching into genpy generated message classes, to set initialized_ field properly
     msg_mod.__init__ = init_punch
 
+#
+# default data values extracted from genpy.generator:default_value()
+#
 
-for msg_mod in [
-    opt_empty,
-    opt_bool,
+for msg_int_mod in [
     opt_int8, opt_int16, opt_int32, opt_int64,
     opt_uint8, opt_uint16, opt_uint32, opt_uint64,
-    opt_float32, opt_float64,
-    opt_string,
-    opt_time,
-    opt_duration,
-    opt_header,
 ]:
-    duck_punch(msg_mod)
+    duck_punch(msg_int_mod, 0)
+
+for msg_float_mod in [
+    opt_float32, opt_float64,
+]:
+    duck_punch(msg_float_mod, 0.)
+
+duck_punch(opt_string, '')
+duck_punch(opt_bool, False)
+duck_punch(opt_empty)  # default value should be unused here
+
+duck_punch(opt_time, genpy.Time())
+duck_punch(opt_duration, genpy.Duration())
+duck_punch(opt_header, std_msgs.msg.Header())
---
+++ tests/test_opt_string.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+try:
+    import genpy
+    import pyros_msgs
+except ImportError:
+    # Because we need to access Ros message types here (from ROS env or from virtualenv, or from somewhere else)
+    import pyros_setup
+    # We rely on default configuration to point us ot the proper distro
+    pyros_setup.configurable_import().configure().activate()
+    import genpy
+    import pyros_msgs
+
+import nose
+
+
+def test_init_data():
+    msg = pyros_msgs.opt_string(data='fortytwo')
+    assert msg.initialized_ is True
+    assert msg.data == 'fortytwo'
+
+
+def test_init_raw():
+    msg = pyros_msgs.opt_string('fortytwo')
+    assert msg.initialized_ is True
+    assert msg.data == 'fortytwo'
+
+
+def test_init_default():
+    msg = pyros_msgs.opt_string()
+    assert msg.initialized_ is False
+    assert msg.data == ""
+
+
+def test_force_init_excepts():
+    with nose.tools.assert_raises(AttributeError) as cm:
+        pyros_msgs.opt_string(initialized_=True)
+    assert cm.exception.message == "The field 'initialized_' is an internal field of pyros_msgs/opt_string and should not be set by the user."
+
+
+# Just in case we run this directly
+if __name__ == '__main__':
+    nose.runmodule(__name__)
