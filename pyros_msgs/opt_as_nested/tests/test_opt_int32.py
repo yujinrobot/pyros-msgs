626bfc59eb1468f553e17fc5ea8a884635851f8c
Thomas <tkostas75@gmail.com>
2017-04-17 10:34:28 +0900

    Adapted all int type size tests

--- pyros_msgs/opt_as_nested/tests/test_opt_int32.py
+++ pyros_msgs/opt_as_nested/tests/test_opt_int32.py
@@ -6,26 +6,21 @@ import os
 import sys
 
 
-# TODO : find a better place for this ?
-import pyros_msgs
-from pyros_msgs.importer.rosmsg_generator import generate_msgsrv_nspkg, import_msgsrv
+# generating all and accessing the required message class.
+from pyros_msgs.opt_as_nested.tests import msg_generate
 
-# a dynamically generated message type just for testing...
-generated_modules = generate_msgsrv_nspkg(
-    [os.path.join(os.path.dirname(__file__), 'msg', 'test_opt_int32_as_nested.msg')],
-    # we need to specify any dependency to have a chance to get it.
-    dependencies=['pyros_msgs'],
-    include_path=['pyros_msgs:{0}'.format(os.path.join(os.path.dirname(pyros_msgs.__path__[0]), 'msg'))],
-    ns_pkg=True
-)
-import_msgsrv('gen_msgs.msg._test_opt_int32_as_nested')
+try:
+    # This should succeed if the message class was already generated
+    import pyros_msgs.msg as pyros_msgs
+except ImportError:  # we should enter here if the message was not generated yet.
+    pyros_msgs = msg_generate.generate_pyros_msgs()
 
-test_opt_int32_as_nested = getattr(sys.modules['gen_msgs.msg._test_opt_int32_as_nested'], 'test_opt_int32_as_nested')
+test_gen_msgs, test_gen_srvs = msg_generate.generate_test_msgs()
 
 
 import pyros_msgs.opt_as_nested
 # patching (need to know the field name)
-pyros_msgs.opt_as_nested.duck_punch(test_opt_int32_as_nested, ['data'])
+pyros_msgs.opt_as_nested.duck_punch(test_gen_msgs.test_opt_int32_as_nested, ['data'])
 
 import pytest
 import hypothesis
@@ -35,27 +30,27 @@ import hypothesis.strategies
 @hypothesis.given(hypothesis.strategies.integers(min_value=-2147483648, max_value=2147483647))
 def test_init_rosdata(data):
     """Testing that a proper data is stored as is"""
-    msg = test_opt_int32_as_nested(data=data)
+    msg = test_gen_msgs.test_opt_int32_as_nested(data=data)
     assert msg.data == data
 
 
 @hypothesis.given(hypothesis.strategies.integers(min_value=-2147483648, max_value=2147483647))
 def test_init_data(data):
     """Testing that an implicitely convertible data is stored as expected"""
-    msg = test_opt_int32_as_nested(data=data)
+    msg = test_gen_msgs.test_opt_int32_as_nested(data=data)
     assert msg.data == data
 
 
 @hypothesis.given(hypothesis.strategies.integers(min_value=-2147483648, max_value=2147483647))
 def test_init_raw(data):
     """Testing storing of data without specifying the field"""
-    msg = test_opt_int32_as_nested(data)
+    msg = test_gen_msgs.test_opt_int32_as_nested(data)
     assert msg.data == data
 
 
 def test_init_default():
     """Testing default value"""
-    msg = test_opt_int32_as_nested()
+    msg = test_gen_msgs.test_opt_int32_as_nested()
     assert msg.data is None
 
 
@@ -68,7 +63,7 @@ def test_init_default():
 def test_wrong_init_except(data):
     """Testing we except when types do not match"""
     with pytest.raises(AttributeError) as cm:
-        test_opt_int32_as_nested(data)
+        test_gen_msgs.test_opt_int32_as_nested(data)
     assert isinstance(cm.value, AttributeError)
     assert "does not match the accepted type schema for 'data' : Any of set" in cm.value.message
 
