6125dd356df8f86f457d188a4bcec14ecf733116
AlexV <asmodehn@gmail.com>
2017-06-16 10:30:25 +0900

    WIP beginning of custom importer implementation

--- pyros_msgs/importer/rosmsg_importer.py
+++ pyros_msgs/importer/rosmsg_importer.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
+import imp
+
+import py
+
 """
 A module to setup custom importer for .msg and .srv files
 Upon import, it will first find the .msg file, then generate the python module for it, then load it.
@@ -11,13 +15,15 @@ TODO...
 # Ref : https://docs.python.org/dev/library/importlib.html#importlib.import_module
 
 # Ref : http://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
-# Note : Coudlnt find a way to make imp.load_source deal with packages or relative imports (necessary for our generated message classes)
+# Note : Couldn't find a way to make imp.load_source deal with packages or relative imports (necessary for our generated message classes)
 import os
 import sys
 
 # This will take the ROS distro version if ros has been setup
 import genpy.generator
 import genpy.generate_initpy
+
+import logging
 
 
 class ROSLoader(object):
@@ -38,33 +44,297 @@ class ROSLoader(object):
 
         return
 
+# https://pymotw.com/2/sys/imports.html#sys-imports
+class NoisyImportFinder(object):
+    PATH_TRIGGER = 'NoisyImportFinder_PATH_TRIGGER'
 
-class ROSFinder(object):
+    def __init__(self, path_entry):
+        print('Checking NoisyImportFinder support for %s' % path_entry)
+        if path_entry != self.PATH_TRIGGER:
+            print('NoisyImportFinder does not work for %s' % path_entry)
+            raise ImportError()
+        return
 
-    def __init__(self):
-        self.loaders = {
-            '.srv': ROSLoader(genpy.generator.SrvGenerator(), 'srv'),
-            '.msg': ROSLoader(genpy.generator.MsgGenerator(), 'msg')
-        }
+    def find_module(self, fullname, path=None):
+        print('NoisyImportFinder looking for "%s"' % fullname)
+        return None
 
-    def find_module(self, name, path=None):
-        """
-        Return the loader for the specified module.
-        """
-        # Ref : https://www.python.org/dev/peps/pep-0302/#specification-part-1-the-importer-protocol
+# https://pymotw.com/3/sys/imports.html#sys-imports
 
-        loader = None
+# class NoisyImportFinder:
+#
+#     PATH_TRIGGER = 'NoisyImportFinder_PATH_TRIGGER'
+#
+#     def __init__(self, path_entry):
+#         print('Checking {}:'.format(path_entry), end=' ')
+#         if path_entry != self.PATH_TRIGGER:
+#             print('wrong finder')
+#             raise ImportError()
+#         else:
+#             print('works')
+#         return
+#
+#     def find_module(self, fullname, path=None):
+#         print('Looking for {!r}'.format(fullname))
+#         return None
+#
+#
+# sys.path_hooks.append(NoisyImportFinder)
 
-        path = path or sys.path
-        for p in path:
-            for f in os.listdir(p):
-                filename, ext = os.path.splitext(f)
-                # our modules generated from messages are always a leaf in import tree so we only care about this case
-                if ext in self.loaders.keys() and filename == name.split('.')[-1]:
-                    loader = self.loaders.get(ext)
-                    break  # we found it. break out.
-
-        return loader
 
 
-sys.meta_path += [ROSFinder()]
+
+
+if sys.version_info >= (3, ):
+    class ROSImportFinder(object):
+
+        PATH_TRIGGER = 'ROSFinder_PATH_TRIGGER'
+
+        def __init__(self, path_entry):
+
+            self.logger = logging.getLogger(__name__)
+            self.logger.debug('Checking ROSImportFinder support for %s' % path_entry)
+            if path_entry != self.PATH_TRIGGER:
+                self.logger.debug('ROSImportFinder does not work for %s' % path_entry)
+                raise ImportError()
+
+            self.loaders = {
+                '.srv': ROSLoader(genpy.generator.SrvGenerator(), 'srv'),
+                '.msg': ROSLoader(genpy.generator.MsgGenerator(), 'msg')
+            }
+
+        def find_module(self, fullname, path=None):
+            print('ROSImportFinder looking for "%s"' % fullname)
+            return None
+
+elif sys.version_info >= (2, 7, 12):
+    class ROSImportFinder(object):
+        PATH_TRIGGER = 'ROSFinder_PATH_TRIGGER'
+
+        def __init__(self, path_entry):
+            self.logger = logging.getLogger(__name__)
+            self.logger.debug('Checking ROSImportFinder support for %s' % path_entry)
+            if path_entry != self.PATH_TRIGGER:
+                self.logger.debug('ROSImportFinder does not work for %s' % path_entry)
+                raise ImportError()
+
+            self.loaders = {
+                '.srv': ROSLoader(genpy.generator.SrvGenerator(), 'srv'),
+                '.msg': ROSLoader(genpy.generator.MsgGenerator(), 'msg')
+            }
+
+        def find_module(self, fullname, path=None):
+            print('ROSImportFinder looking for "%s"' % fullname)
+            return None
+
+else:
+    class ROSImportFinder(object):
+
+        PATH_TRIGGER = 'ROSFinder_PATH_TRIGGER'
+
+        def __init__(self, path_entry=None):
+            self.logger = logging.getLogger(__name__)
+            self.logger.debug('Checking ROSImportFinder support for %s' % path_entry)
+            if path_entry is None:  # called on very old python (< 2.7.12)
+                pass
+            elif path_entry != self.PATH_TRIGGER:  # python following correct PEP ( 302 ?)
+                self.logger.debug('ROSImportFinder does not work for %s' % path_entry)
+                raise ImportError()
+
+            self.loaders = {
+                '.srv': ROSLoader(genpy.generator.SrvGenerator(), 'srv'),
+                '.msg': ROSLoader(genpy.generator.MsgGenerator(), 'msg')
+            }
+
+        def find_module(self, name, path=None):
+            self.logger.debug('ROSImportFinder looking for "%s"' % name)
+
+            # implementation inspired from pytest.rewrite
+            names = name.rsplit(".", 1)
+            lastname = names[-1]
+            pth = None
+            if path is not None:
+                # Starting with Python 3.3, path is a _NamespacePath(), which
+                # causes problems if not converted to list.
+                path = list(path)
+                if len(path) == 1:
+                    pth = path[0]
+
+
+            if pth is None:
+
+
+                try:
+                    fd, fn, desc = imp.find_module(lastname, path)
+                except ImportError:
+                    return None
+                if fd is not None:
+                    fd.close()
+
+
+
+
+                tp = desc[2]
+                if tp == imp.PY_COMPILED:
+                    if hasattr(imp, "source_from_cache"):
+                        try:
+                            fn = imp.source_from_cache(fn)
+                        except ValueError:
+                            # Python 3 doesn't like orphaned but still-importable
+                            # .pyc files.
+                            fn = fn[:-1]
+                    else:
+                        fn = fn[:-1]
+                elif tp != imp.PY_SOURCE:
+                    # Don't know what this is.
+                    return None
+            else:
+                fn = os.path.join(pth, name.rpartition(".")[2] + ".py")
+
+            # fn_pypath = py.path.local(fn)
+            # if not self._should_rewrite(name, fn_pypath, state):
+            #     return None
+            #
+            # self._rewritten_names.add(name)
+
+    # def find_module(self, name, path=None):
+    #     """
+    #     Return the loader for the specified module.
+    #     """
+    #     # Ref : https://www.python.org/dev/peps/pep-0302/#specification-part-1-the-importer-protocol
+    #
+    #     #
+    #     loader = None
+    #
+    #     # path = path or sys.path
+    #     # for p in path:
+    #     #     for f in os.listdir(p):
+    #     #         filename, ext = os.path.splitext(f)
+    #     #         # our modules generated from messages are always a leaf in import tree so we only care about this case
+    #     #         if ext in self.loaders.keys() and filename == name.split('.')[-1]:
+    #     #             loader = self.loaders.get(ext)
+    #     #             break  # we found it. break out.
+    #     #
+    #     # return loader
+    #
+    #     # implementation inspired from pytest.rewrite
+    #     self.logger.debug("find_module called for: %s" % name)
+    #     names = name.rsplit(".", 1)
+    #     lastname = names[-1]
+    #     pth = None
+    #     if path is not None:
+    #         # Starting with Python 3.3, path is a _NamespacePath(), which
+    #         # causes problems if not converted to list.
+    #         path = list(path)
+    #         if len(path) == 1:
+    #             pth = path[0]
+    #
+    #
+    #     if pth is None:
+    #
+    #
+    #
+    #
+    #
+    #         try:
+    #             fd, fn, desc = imp.find_module(lastname, path)
+    #         except ImportError:
+    #             return None
+    #         if fd is not None:
+    #             fd.close()
+    #
+    #
+    #
+    #
+    #         tp = desc[2]
+    #         if tp == imp.PY_COMPILED:
+    #             if hasattr(imp, "source_from_cache"):
+    #                 try:
+    #                     fn = imp.source_from_cache(fn)
+    #                 except ValueError:
+    #                     # Python 3 doesn't like orphaned but still-importable
+    #                     # .pyc files.
+    #                     fn = fn[:-1]
+    #             else:
+    #                 fn = fn[:-1]
+    #         elif tp != imp.PY_SOURCE:
+    #             # Don't know what this is.
+    #             return None
+    #     else:
+    #         fn = os.path.join(pth, name.rpartition(".")[2] + ".py")
+    #
+    #     fn_pypath = py.path.local(fn)
+    #     if not self._should_rewrite(name, fn_pypath, state):
+    #         return None
+    #
+    #     self._rewritten_names.add(name)
+    #
+    #     # The requested module looks like a test file, so rewrite it. This is
+    #     # the most magical part of the process: load the source, rewrite the
+    #     # asserts, and load the rewritten source. We also cache the rewritten
+    #     # module code in a special pyc. We must be aware of the possibility of
+    #     # concurrent pytest processes rewriting and loading pycs. To avoid
+    #     # tricky race conditions, we maintain the following invariant: The
+    #     # cached pyc is always a complete, valid pyc. Operations on it must be
+    #     # atomic. POSIX's atomic rename comes in handy.
+    #     write = not sys.dont_write_bytecode
+    #     cache_dir = os.path.join(fn_pypath.dirname, "__pycache__")
+    #     if write:
+    #         try:
+    #             os.mkdir(cache_dir)
+    #         except OSError:
+    #             e = sys.exc_info()[1].errno
+    #             if e == errno.EEXIST:
+    #                 # Either the __pycache__ directory already exists (the
+    #                 # common case) or it's blocked by a non-dir node. In the
+    #                 # latter case, we'll ignore it in _write_pyc.
+    #                 pass
+    #             elif e in [errno.ENOENT, errno.ENOTDIR]:
+    #                 # One of the path components was not a directory, likely
+    #                 # because we're in a zip file.
+    #                 write = False
+    #             elif e in [errno.EACCES, errno.EROFS, errno.EPERM]:
+    #                 state.trace("read only directory: %r" % fn_pypath.dirname)
+    #                 write = False
+    #             else:
+    #                 raise
+    #     cache_name = fn_pypath.basename[:-3] + PYC_TAIL
+    #     pyc = os.path.join(cache_dir, cache_name)
+    #     # Notice that even if we're in a read-only directory, I'm going
+    #     # to check for a cached pyc. This may not be optimal...
+    #     co = _read_pyc(fn_pypath, pyc, state.trace)
+    #     if co is None:
+    #         state.trace("rewriting %r" % (fn,))
+    #         source_stat, co = _rewrite_test(self.config, fn_pypath)
+    #         if co is None:
+    #             # Probably a SyntaxError in the test.
+    #             return None
+    #         if write:
+    #             _make_rewritten_pyc(state, source_stat, pyc, co)
+    #     else:
+    #         state.trace("found cached rewritten pyc for %r" % (fn,))
+    #     self.modules[name] = co, pyc
+    #     return self
+
+_ros_finder_instance_obsolete_python = ROSImportFinder
+
+
+def activate():
+    if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
+        sys.path_hooks.append(ROSImportFinder)
+    else:  # older (trusty) version
+        sys.path_hooks.append(_ros_finder_instance_obsolete_python)
+
+    for hook in sys.path_hooks:
+        print('Path hook: {}'.format(hook))
+
+    sys.path.insert(0, ROSImportFinder.PATH_TRIGGER)
+
+
+def deactivate():
+    if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
+        sys.path_hooks.remove(ROSImportFinder)
+    else:  # older (trusty) version
+        sys.path_hooks.remove(_ros_finder_instance_obsolete_python)
+
+    sys.path.remove(ROSImportFinder.PATH_TRIGGER)
--- pyros_msgs/importer/tests/test_rosmsg_generator_import.py
+++ pyros_msgs/importer/tests/test_rosmsg_generator_import.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import importlib
+
 """
 Testing executing rosmsg_generator directly (like setup.py would)
 """
@@ -9,28 +11,95 @@ import runpy
 
 # Ref : http://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
 
-# Importing generator module
-from pyros_msgs.importer import rosmsg_generator
+# Importing importer module
+from pyros_msgs.importer import rosmsg_importer
 
 
-def test_generate_msgsrv_nspkg_usable():
-    # generating message class
-    sitedir, generated_msg, generated_srv = rosmsg_generator.generate_msgsrv_nspkg(
-        [os.path.join(os.path.dirname(__file__), 'msg', 'TestMsg.msg')],
-        package='test_gen_msgs',
-        ns_pkg=True,
-    )
+def setup_module():
+    rosmsg_importer.activate()
 
 
+def teardown_module():
+    rosmsg_importer.deactivate()
+
+
+# importlib
+# https://pymotw.com/3/importlib/index.html
+# https://pymotw.com/2/importlib/index.html
+
+def test_importlib_msg_module_absolute():
     # Verify that files exists and are importable
-    for m in [generated_msg, generated_srv]:
-        # modules are generated where the file is launched
-        gen_file = os.path.join(sitedir, *m.split("."))
-        assert os.path.exists(gen_file + '.py') or os.path.exists(os.path.join(gen_file, '__init__.py'))
+    if hasattr(importlib, 'find_loader'):  # recent version of import lib
 
-        msg_mod, srv_mod = rosmsg_generator.import_msgsrv(sitedir, generated_msg, generated_srv)
+        pkg_loader = importlib.find_loader('test_gen_msgs')
+        pkg = pkg_loader.load_module()
 
-        assert msg_mod is not None
-        assert srv_mod is not None
+        subpkg_loader = importlib.find_loader('msg', pkg.__path__)
+        subpkg = subpkg_loader.load_module()
 
+        loader = importlib.find_loader('TestMsg', subpkg.__path__)
+        msg_mod = subpkg_loader.load_module()
+
+    else:  # old version
+        msg_mod = importlib.import_module('pyros_msgs.importer.tests.msg.TestMsg')
+
+    if hasattr(importlib, 'reload'):  # recent version of importlib
+        # attempting to reload
+        importlib.reload(msg_mod)
+    else:
+        pass
+
+
+
+# def test_importlib_msg_module_relative():
+#     # Verify that files exists and are importable
+#     # TODO
+#     # if hasattr(importlib, 'find_loader'):  # recent version of import lib
+#     #
+#     #     pkg_loader = importlib.find_loader('test_gen_msgs')
+#     #     pkg = pkg_loader.load_module()
+#     #
+#     #     subpkg_loader = importlib.find_loader('msg', pkg.__path__)
+#     #     subpkg = subpkg_loader.load_module()
+#     #
+#     #     loader = importlib.find_loader('TestMsg', subpkg.__path__)
+#     #     m = subpkg_loader.load_module()
+#     #
+#     # else:  # old version
+#     msg_mod = importlib.import_module('.msg.TestMsg', package=__package__)
+
+
+
+
+def test_importlib_srv_module():
+    pass
+    # TODO
+    # # Verify that files exists and are importable
+    # msg_mod = importlib.import_module('test_gen_msgs.srv.TestSrv')
+
+
+# imp https://pymotw.com/2/imp/index.html
+# TODO
+
+# def test_imp_msg_module():
+#     # Verify that files exists and are importable
+#     msg_mod = imp.import_module('test_gen_msgs.msg.TestMsg')
+#
+#
+# def test_imp_msg_pkg():
+#     # Verify that files exists and are importable
+#     msg_mod = imp.import_module('test_gen_msgs.msg')
+#
+#
+# def test_imp_srv_module():
+#     # Verify that files exists and are importable
+#     msg_mod = imp.import_module('test_gen_msgs.srv.TestSrv')
+#
+#
+# def test_imp_srv_pkg():
+#     # Verify that files exists and are importable
+#     msg_mod = imp.import_module('test_gen_msgs.srv')
+
+
+# TODO : different python versions...
 
