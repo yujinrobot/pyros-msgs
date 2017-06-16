06390f86aa092d2b4423d486d510d97a19cc3dc7
AlexV <asmodehn@gmail.com>
2017-06-16 16:06:53 +0900

    WIP importer improvements

--- pyros_msgs/importer/rosmsg_importer.py
+++ pyros_msgs/importer/rosmsg_importer.py
@@ -28,27 +28,128 @@ import genpy.generate_initpy
 import logging
 
 
+def _mk_init_name(fullname):
+    """Return the name of the __init__ module for a given package name."""
+    if fullname.endswith('.__init__'):
+        return fullname
+    return fullname + '.__init__'
+
+
+def _get_key_name(fullname, db):
+    """Look in an open shelf for fullname or fullname.__init__, return the name found."""
+    if fullname in db:
+        return fullname
+    init_name = _mk_init_name(fullname)
+    if init_name in db:
+        return init_name
+    return None
+
+
 class ROSLoader(object):
 
-    def __init__(self, path_entry):
+    def __init__(self, path_entry, msgsrv_files, package, outdir_pkg, includepath = None, ns_pkg = None):
+
+        self.logger = logging.getLogger(__name__)
         self.path_entry = path_entry
 
-    # def __init__(self, generator, ext):
-    #     self.ext = ext
-    #     self.generator = generator
+        self.msgsrv_files = msgsrv_files
+        self.package = package
+        self.outdir_pkg = outdir_pkg
+
+        # TODO : we need to determine that from the loader
+        self.includepath = includepath
+        self.ns_pkg = ns_pkg
+
+
+    def _generate(self, fullname):
+
+        gen = rosmsg_generator.genmsgsrv_py(
+            msgsrv_files=self.msgsrv_files,
+            package=self.package,
+            outdir_pkg=self.outdir_pkg,
+            includepath=self.includepath,
+            ns_pkg=self.ns_pkg
+        )
+        return gen
+
+    def get_source(self, fullname):
+        self.logger.debug('loading source for "%s" from msg' % fullname)
+        try:
+
+            self._generate()
+
+            # with shelve_context(self.path_entry) as db:
+            #     key_name = _get_key_name(fullname, db)
+            #     if key_name:
+            #         return db[key_name]
+            #     raise ImportError('could not find source for %s' % fullname)
+
+            pass
+
+
+        except Exception as e:
+            self.logger.debug('could not load source:', e)
+            raise ImportError(str(e))
+
 
     # defining this to benefit from backward compat import mechanism in python 3.X
     def get_filename(self, name):
         os.sep.join(name.split(".")) + '.' + self.ext
 
+    def _get_filename(self, fullname):
+        # Make up a fake filename that starts with the path entry
+        # so pkgutil.get_data() works correctly.
+        return os.path.join(self.path_entry, fullname)
+
     # defining this to benefit from backward compat import mechanism in python 3.X
     def is_package(self, name):
+        names = name.split(".")
+        parent_idx = len(names) -1
+        # trying to find a parent already loaded
+        while 0<= parent_idx < len(names) :
+            if names[parent_idx] in sys.modules: # we found a parent, we can get his path and go back
+                pass
+            else:  # parent not found, need to check its parent
+                parent_idx-=1
+
+
+        else:  # No loaded parent found, lets attempt to import it directly (searching in all sys.paths)
+
+            pass
+
+
         return None  # TODO : implement check
 
     def load_module(self, fullname):
 
+        if fullname in sys.modules:
+            self.logger.debug('reusing existing module from previous import of "%s"' % fullname)
+            mod = sys.modules[fullname]
+        else:
+            self.logger.debug('creating a new module object for "%s"' % fullname)
+            mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
 
-        return
+        # Set a few properties required by PEP 302
+        mod.__file__ = self._get_filename(fullname)
+        mod.__name__ = fullname
+        mod.__path__ = self.path_entry
+        mod.__loader__ = self
+        mod.__package__ = '.'.join(fullname.split('.')[:-1])
+
+        if self.is_package(fullname):
+            self.logger.debug('adding path for package')
+            # Set __path__ for packages
+            # so we can find the sub-modules.
+            mod.__path__ = [self.path_entry]
+        else:
+            self.logger.debug('imported as regular module')
+
+        source = self.get_source(fullname)
+
+        self.logger.debug('execing source...')
+        exec source in mod.__dict__
+        self.logger.debug('done')
+        return mod
 
 # https://pymotw.com/3/sys/imports.html#sys-imports
 
@@ -78,7 +179,7 @@ class ROSLoader(object):
 
 if sys.version_info >= (3, ):
     class ROSImportFinder(object):
-
+        # TODO : we can use this to enable ROS Finder only on relevant paths (ros distro paths + dev workspaces) from sys.path
         PATH_TRIGGER = 'ROSFinder_PATH_TRIGGER'
 
         def __init__(self, path_entry):
@@ -124,10 +225,8 @@ else:
         def __init__(self, path_entry=None):
             # First we need to skip all the cases that we are not concerned with
 
-            if path_entry is None:  # called on obsolete python (< 2.7.12)
-                pass
-            # else path_entry contains the path where the finder has been instanciated...
-            elif not os.path.exists(os.path.join(path_entry, 'msg')) and not os.path.exists(os.path.join(path_entry, 'srv')):
+            # path_entry contains the path where the finder has been instantiated...
+            if not os.path.exists(os.path.join(path_entry, 'msg')) and not os.path.exists(os.path.join(path_entry, 'srv')):
                 raise ImportError  # we raise if we cannot find msg or srv folder
 
             # Then we can do the initialisation
@@ -137,13 +236,12 @@ else:
             self.path_entry = path_entry
 
 
-
+        # This is called for the first unknown (not loaded) name inside path
         def find_module(self, name, path=None):
             self.logger.debug('ROSImportFinder looking for "%s"' % name)
 
             # on 2.7
             path = path or self.path_entry
-
 
             # implementation inspired from pytest.rewrite
             names = name.rsplit(".", 1)
@@ -157,7 +255,14 @@ else:
                 rosf = [f for f in os.listdir(os.path.join(path, lastname)) if os.path.splitext(f)[-1] in ['.msg', '.srv']]
 
                 if rosf:
-                    return ROSLoader(path_entry=os.path.join(path, lastname))
+                    return ROSLoader(
+                        msgsrv_files=rosf,
+                        path_entry=os.path.join(path, lastname),
+                        package=name,
+                        outdir_pkg=path,  # TODO: if we cannot write into source, use tempfile
+                        #includepath=,
+                        #ns_pkg=,
+                    )
 
                 # # package case
                 # for root, dirs, files in  os.walk(path, topdown=True):
@@ -181,7 +286,14 @@ else:
 
             elif os.path.isfile(os.path.join(path, lastname)):
                 # module case
-                pass
+                return ROSLoader(
+                    msgsrv_files=rosf,
+                    path_entry=os.path.join(path, lastname),
+                    package=name,
+                    outdir_pkg=path,  # TODO: if we cannot write into source, use tempfile
+                    # includepath=,
+                    # ns_pkg=,
+                )
 
             return None
 
@@ -308,25 +420,27 @@ else:
     #     self.modules[name] = co, pyc
     #     return self
 
-_ros_finder_instance_obsolete_python = ROSImportFinder
+#_ros_finder_instance_obsolete_python = ROSImportFinder
 
 
 def activate():
-    if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
-        sys.path_hooks.append(ROSImportFinder)
-    else:  # older (trusty) version
-        sys.path_hooks.append(_ros_finder_instance_obsolete_python)
+    #if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
+    sys.path_hooks.append(ROSImportFinder)
+    # else:  # older (trusty) version
+    #     sys.path_hooks.append(_ros_finder_instance_obsolete_python)
 
     for hook in sys.path_hooks:
         print('Path hook: {}'.format(hook))
 
+    # TODO : mix that with ROS PYTHONPATH shenanigans... to enable the finder only for 'ROS aware' paths
     #sys.path.insert(0, ROSImportFinder.PATH_TRIGGER)
 
 
 def deactivate():
-    if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
-        sys.path_hooks.remove(ROSImportFinder)
-    else:  # older (trusty) version
-        sys.path_hooks.remove(_ros_finder_instance_obsolete_python)
+    #if sys.version_info >= (2, 7, 12):  # TODO : which exact version matters ?
+    sys.path_hooks.remove(ROSImportFinder)
+    # else:  # older (trusty) version
+    #     sys.path_hooks.remove(_ros_finder_instance_obsolete_python)
 
+    # TODO : mix that with ROS PYTHONPATH shenanigans... to enable the finder only for 'ROS aware' paths
     #sys.path.remove(ROSImportFinder.PATH_TRIGGER)
